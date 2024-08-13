962dfd9fd707b750f19db26d10a5b9e5475c5848
yassinenakkabi <yassinenakkabi@gmail.comgit config --global user.email yassinenakkabi@gmail.com>
2024-08-13 19:25:54 +0100

    create page createpost + add file utils and index.s

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 function CreatePost() {
+  const navigate = useNavigate()
+  const [form, setForm]=useState({
+    name: "",
+    prompt: "",
+    photo:"",
+  })
+  const [generatingImg, setGeneratingImg]=useState(false)
+  const [loading, setLoading]=useState(false)
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+      </div>
+
+    </section>
   )
 }
 
