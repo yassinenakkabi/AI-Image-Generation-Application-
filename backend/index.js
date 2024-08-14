1852ff3316c0eb5ba9b1c60f74d61b65775f013e
yassinenakkabi <yassinenakkabi@gmail.comgit config --global user.email yassinenakkabi@gmail.com>
2024-08-14 01:39:37 +0100

    integration MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,33 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
-
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'})) ;
+app.use(express.json({ limit: '50mb' }));
 
-app.get('/', async (req, res)=>{
-    res.send('Hello from DELL-E');
-})
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL-E!',
+  });
+});
 
 const startServer = async () => {
-    app.listen(8000, ()=> console.log("Server has started on port http://localhost:8000"))
-}
-startServer();
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+startServer();
