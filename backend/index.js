91ee5fa5f88ba415700cf1593de932d4ea09722e
yassinenakkabi <yassinenakkabi@gmail.com>
2024-08-15 13:42:32 +0100

    connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -12,22 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+// app.use('/api/v1/post', postRoutes);
+// app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'Hello from DALL-E!',
-  });
+  // res.status(200).json({
+  res.send('Hello from DALL-E!')
+  // });
 });
 
 const startServer = async () => {
-  try {
+  // try {
     connectDB(process.env.MONGODB_URL);
     app.listen(8080, () => console.log('Server started on port 8080'));
-  } catch (error) {
-    console.log(error);
-  }
+  // } catch (error) {
+  //   console.log(error);
+  // }
 };
 
 startServer();
